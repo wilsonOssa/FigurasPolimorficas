bcb627d784f5e772bb01665e2b2ed848d1c0a6ff
wilsonOssa <wilson.ossa974@pascualbravo.edu.co>
2025-04-03 16:11:36 -0500

    Actualización: Implementado FiguraFactory

---
+++ FigurasPolimorficas/FiguraFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+namespace FigurasPolimorficas
+{
+    public static class FiguraFactory
+    {
+        public static Figura CrearFigura(string tipo, Color color, Point posicion)
+        {
+            switch (tipo)
+            {
+                case "Círculo":
+                    return new Circulo(color, posicion, 50);
+                case "Línea":
+                    return new Linea(color, posicion, new Point(posicion.X + 100, posicion.Y));
+                case "Triángulo":
+                    return new Triangulo(color, posicion, 50);
+                default:
+                    return null;
+            }
+        }
+    }
+}
