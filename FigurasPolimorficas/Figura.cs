3f6db96d966eaa380ab080a20843b1fb070c64f1
wilsonOssa <wilson.ossa974@pascualbravo.edu.co>
2025-04-03 16:04:51 -0500

    Agregado: Clase abstracta Figura

---
+++ FigurasPolimorficas/Figura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+
+namespace FigurasPolimorficas
+{
+    public abstract class Figura
+    {
+        public Color Color { get; set; }
+        public Point Posicion { get; set; }
+
+        public Figura(Color color, Point posicion)
+        {
+            Color = color;
+            Posicion = posicion;
+        }
+
+        public abstract void Dibujar(Graphics g);
+    }
+}
